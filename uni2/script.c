cab96895f6e55edb8fd5813cb363b8e41c3149fd
Carlos Gabriel Medeiros da Silva <90879208+CarlosG18@users.noreply.github.com>
2025-06-11 07:38:20 -0600

    Update script.c

--- uni2/script.c
+++ uni2/script.c
@@ -3,8 +3,13 @@
 #include "DHTesp.h"
 #include <PubSubClient.h>
 
+#define LED_ALARME 18
+#define LED_GAS 19
+
 const int DHT_PIN = 15;
 const int PIN_PRESENCA = 4;
+
+bool gas_on = 1;
 
 DHTesp dhtSensor;
 
@@ -26,11 +31,32 @@ WiFiClient espClient;
 
 PubSubClient client(espClient);
 
+void callback(char* topic, byte* message, unsigned int length) {
+  String msg;
+
+  for (int i = 0; i < length; i++) {
+    msg += (char)message[i];
+  }
+  Serial.print("Mensagem recebida [");
+  Serial.print(topic);
+  Serial.print("]: ");
+  Serial.println(msg);
+
+  if (String(topic) == "/esp-iot/gas") {
+    if (msg == "true") {
+      gas_on = true;
+    } else if (msg == "false") {
+      gas_on = false;
+    }
+  }
+}
+
 void reconnect(){
    while (!client.connected()) {
     Serial.print("Tentando conexão MQTT...");
     if (client.connect(id, mqtt_user, mqtt_pass)) {
       Serial.println("Conectado ao broker MQTT!");
+      client.subscribe("/esp-iot/gas");
     } else {
       Serial.print("Falha na conexão, rc=");
       Serial.print(client.state());
@@ -61,6 +87,10 @@ void setup_wifi() {
 }
 
 void setup() {
+  // definindo os leds
+  pinMode(LED_ALARME, OUTPUT);
+  pinMode(LED_GAS, OUTPUT);
+
   Serial.begin(115200);
   pinMode(PIN_PRESENCA, INPUT);
   dhtSensor.setup(DHT_PIN, DHTesp::DHT22);
@@ -68,33 +98,45 @@ void setup() {
   setup_wifi();
 
   client.setServer(mqtt_server, mqtt_port);
-
+  // cadastrando a função de callback
+  client.setCallback(callback);
 }
 
 void loop() {
+  if(gas_on){
+    digitalWrite(LED_GAS, HIGH);
+  }else{
+    digitalWrite(LED_GAS, LOW);
+  }
+
+  Serial.println(gas_on);
   
   if(!client.connected()){
     reconnect();
   }
 
   TempAndHumidity  data = dhtSensor.getTempAndHumidity();
-  Serial.println("Temp: " + String(data.temperature, 2) + "°C");
-  Serial.println("Humidity: " + String(data.humidity, 1) + "%");
-  Serial.println("---");
-  String payload = "Temp: " + String(data.temperature, 2) + "°C" + "Humidity: " + String(data.humidity, 1) + "%" ;
-  client.publish ("/esp-iot", payload.c_str());
+  //Serial.println("Temp: " + String(data.temperature, 2) + "°C");
+  //Serial.println("Humidity: " + String(data.humidity, 1) + "%");
+  //Serial.println("---");
+  //String payload = "Temp: " + String(data.temperature, 2) + "°C" + "Humidity: " + String(data.humidity, 1) + "%" ;
+  //String Temp = "Temp: " + String(data.temperature, 2) + "°C";
+  String Temp = String(data.temperature, 2);
+  String Humidity = String(data.humidity, 1);
+  //client.publish ("/esp-iot", payload.c_str());
+  client.publish ("/esp-iot/temp", Temp.c_str());
+  client.publish ("/esp-iot/hum", Humidity.c_str());
   delay(2000); // Wait for a new reading from the sensor (DHT22 has ~0.5Hz sample rate)
   
   //leitura do sensor de presença
-  
   int value = digitalRead(PIN_PRESENCA);
   if (value == HIGH){
-    Serial.println("foi identificado algum corno!");
-    client.publish("/esp-iot", "Presença detectada!");
+    //Serial.println("foi identificado algum corno!");
+    client.publish("/esp-iot/pres", "1");
     delay(2000);
   }else{
-    Serial.println("Nenhum chifre a vista!");
-    client.publish("/esp-iot", "ninguém agora");
+    //Serial.println("Nenhum chifre a vista!");
+    client.publish("/esp-iot/pres", "0");
     delay(2000);
   }
 
